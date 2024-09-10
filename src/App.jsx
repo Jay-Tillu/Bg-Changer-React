07a2ef6a82d7a93f5fd6d70d38decac0dc19cc2a
Jay Tillu <26956655+Jay-Tillu@users.noreply.github.com>
2024-09-10 14:39:43 +0530

    Bg Changer Project Added

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+
+function App() {
+  const [color, setColor] = useState('lavender')
+  return (
+    <>
+      <div className='w-full h-screen duration-200' style={{ backgroundColor: color }}>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
+            <button
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{ backgroundColor: 'red' }}
+              // OnClick is a unique method in React. It expects a function. 
+              // if you want to pass paramenters to function in onClick. It will directly executes it.
+              // So here we give it a callback.
+              onClick={() => { setColor('red') }}
+            >Red</button>
+
+            <button
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{ backgroundColor: 'green' }}
+              onClick={() => { setColor('green') }}
+            >Green</button>
+
+            <button
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{ backgroundColor: 'purple' }}
+              onClick={() => { setColor('purple') }}
+            >Purple</button>
+
+            <button
+              className='outline-none px-4 py-1 rounded-full text-black shadow-lg'
+              style={{ backgroundColor: 'orange' }}
+              onClick={() => { setColor('orange') }}
+            >Orange</button>
+
+            <button
+              className='outline-none px-4 py-1 rounded-full text-white shadow-lg'
+              style={{ backgroundColor: 'olive' }}
+              onClick={() => { setColor('olive') }}
+            >Olive</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
+
